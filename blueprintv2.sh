342a085d2aff220c0eef18c425ceda0ca01e3a33
LaasYath <67027434+LaasYath@users.noreply.github.com>
2024-10-25 09:21:57 -0500

    Update blueprintv2.sh
    add print statements to check script and help with debugging

--- blueprintv2.sh
+++ blueprintv2.sh
@@ -15,6 +15,7 @@ echo "run updates"
   
 #install firewall + configure
 
+  echo "ENABLING FIREWALL, SETTING PORT PERMISSIONS"
   sudo apt-get install ufw -y -qq
   sudo ufw enable
   #tcp/udp specific ports generally seem insecure. udp is insecure, only want tcp
@@ -33,16 +34,18 @@ echo "run updates"
   sudo ufw deny printer
   sudo ufw deny http
 
-  sudo service ssh restart
   clear
 
 #autoclean and autoremove - gets rid of packages that have been downloaded but not installed (simply taking up cache space, could be malware)
 
+  echo "AUTOREMOVE AND AUTOCLEAN PACKAGES"
   sudo apt-get autoremove -y -qq
   sudo apt-get autoclean -y -qq
   sudo apt-get clean -y -qq
 
 #set system file permissions
+
+  echo "SYSTEM FILE PERMISSIONS (ex. bash history)"
   sudo chmod 640 .bash_history
   sudo chmod 604 /etc/shadow
   sudo chmod 644 /etc/hosts
@@ -50,29 +53,38 @@ echo "run updates"
   # sudo chmod 640 /home/$name/bash_history
 
 #disabling guest
+  echo "DISALLOW GUEST"
   sudo echo "allow-guest=false" >> /etc/lightdm/lightdm.conf
 
 #turn on audit policies
+  echo 	"TURN ON AUDIT POLICIES"
   sudo apt-get install auditd -y
   sudo auditctl -e 1 > /var/local/audit.log
 
 #no startup scripts running
+  echo "ENSURE NO START-UP SCRIPTS ARE RUNNING"
   echo > /etc/rc.local
   echo 'exit 0' >> /etc/rc.local
 
 #install ssh server 
+  echo "ALLOW SSH"
   sudo apt-get install openssh-server -y -qq
   sudo ufw allow ssh
   lineNum = "$(grep -n -m 1 PermitRootLogin)"
   sed -e "${lineNum}s/.*/PermitRootLogin no/" /etc/ssh/sshd_config 
+
+  sudo service ssh restart
   
 
 #turn on automatic updates (just security updates, doesn't work for everythin to make sure admin vets updates)
 
+  echo "AUTO SECURITY UPDATES ENABLED"
   sudo apt-get install unattended-upgrades
   sudo dpkg-reconfigure unattended-upgrades
   
 #find all media files and remove
+
+  echo "REMOVE MPR AND MOV FILES"
   sudo find / -name "*.mp3" -type f >> ~/congenial-disco/prohibitedFilesList.log
   sudo find / -name "*.mp4" -type f >> ~/congenial-disco/prohibitedFilesList.log
   sudo find / -name "*.wav" -type f >> ~/congenial-disco/prohibitedFilesList.log
@@ -81,6 +93,8 @@ echo "run updates"
   sudo find / -name "*.php" -type f >> ~/congenial-disco/prohibitedFilesList.log
 
 #remove unwanted malware files and whatnot
+
+  echo "DELETE ALL TYPES OF MALWARE"
   sudo find /bin/ -name "*.sh" -type f -delete # removes any script files from trash
 
   #Remove variances of netcat
@@ -136,8 +150,10 @@ echo "run updates"
   clear
 
 #install a bunch of need anti malware stuff
+
+  echo "INSTALL ANTIMALWARE SOFTWARE"
   sudo apt-get install chkrootkit clamav rkhunter selinux tree auditd
-	clear
+  clear
   #does one line work?
 
 #check netstat ports and daemons -> netstat -tulpn
