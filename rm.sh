78cd2e6408a71fa4729a25db82de351cdc8c5c96
LaasYath <67027434+LaasYath@users.noreply.github.com>
2024-10-25 12:47:57 -0500

    Create rm.sh

---
+++ rm.sh
@@ -0,0 +1,13 @@
+sudo apt-get purge netcat* -y
+sudo apt-get purge ncat* -y
+
+sudo apt-get purge hydra* -y
+
+sudo apt-get purge ophcrack* -y
+
+sudo apt-get purge pdfcrack* -y
+
+sudo apt-get purge apache* -y
+sudo apt-get purge apache2* -y
+
+sudo apt-get purge freeciv* -y
