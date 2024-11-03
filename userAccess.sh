f833f356c0f89a944d57aafc70a49330977f80a7
LaasYath <67027434+LaasYath@users.noreply.github.com>
2024-11-03 17:20:19 -0600

    Update userAccess.sh
    updated post comp image test

--- userAccess.sh
+++ userAccess.sh
@@ -5,27 +5,30 @@ getent passwd | cut -d: -f1 > systemUsers.txt
 sort systemUsers.txt -o systemUsers.txt
 sort approvedUsers.txt -o approvedUsers.txt
 
-toDel = $(comm -13 approvedUsers.txt systemUsers.txt)
+toDel=$(comm -13 approvedUsers.txt systemUsers.txt)
 
 for user in $toDel; do
   userdel $user
+done
 
-toAdd = $(comm -23 approvedUsers.txt systemUsers.txt)
+toAdd=$(comm -23 approvedUsers.txt systemUsers.txt)
 
 for user in $toAdd; do
   adduser $user
+done
 
-getent sudoers | cut -d: -f4 > systemAdmin.txt
-sed 's/,/\n/g' systemAdmin.txt > systemAdmin.txt
+getent group sudo | cut -d: -f4 > systemAdmin.txt
+sed 's/,/\n/g' systemAdmin.txt > tmp.txt && mv tmp.txt systemAdmin.txt
 sort systemAdmin.txt -o systemAdmin.txt
 sort approvedAdmin.txt -o approvedAdmin.txt
 
-toMakeReg = $(comm -13 approvedAdmin.txt systemAdmin.txt)
-for user in $toMakeAdmin; do
+toMakeReg=$(comm -13 approvedAdmin.txt systemAdmin.txt)
+for user in $toMakeReg; do
   gpasswd -d $user sudo
-  
-toMakeAdmin = $(comm -23 approvedUsers.txt systemUsers.txt)
+done
+
+toMakeAdmin=$(comm -23 approvedAdmin.txt systemAdmin.txt)
 
 for user in $toMakeAdmin; do
   usermod -aG sudo $user
-
+done
